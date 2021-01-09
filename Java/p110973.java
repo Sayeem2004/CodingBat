77bcb7855c0f469b4b94309f56a0a66f9b847415
Mohammad Khan <67429452+Sayeem2004@users.noreply.github.com>
2021-01-09 13:00:10 -0500

    Add New Files

---
+++ Java/p110973.java
@@ -0,0 +1,8 @@
+public boolean answerCell(boolean isMorning, boolean isMom, boolean isAsleep) {
+  if (isAsleep) return false;
+  if (isMorning) {
+    if (isMom) return true;
+    return false;
+  }
+  return true;
+}
---
+++ Java/p193613.java
@@ -0,0 +1,3 @@
+public boolean nearTen(int n) {
+  return (n%10 <= 2 || n%10 >= 8);
+}
